86d658d41ff353b1d50bedd1289f305eed2427d7
ignat <ignatov.d43@gmail.com>
2023-12-02 21:17:11 +0300

    Install mongodb, horizon, debugbar and telescope

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (\Illuminate\Http\Request $request) {
+Route::get('/', function () {
     return view('welcome');
 });
 
@@ -23,10 +23,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('destroy');
-    Route::post('/create-token', [ProfileController::class, 'createToken'])->name('createToken');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/create-token', [ProfileController::class, 'createToken'])->name('profile.createToken');
 });
 
 require __DIR__.'/auth.php';
